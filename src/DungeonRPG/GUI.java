c8c4934d71eb6a45b2be63b1bcc884f6e19df0e1
rayesalnasra <130417264+rayesalnasra@users.noreply.github.com>
2024-06-08 19:21:44 +1200

    Made game window centre on player screen on launch

--- src/DungeonRPG/GUI.java
+++ src/DungeonRPG/GUI.java
@@ -233,6 +233,7 @@ public class GUI extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void moveNorthActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_moveNorthActionPerformed
