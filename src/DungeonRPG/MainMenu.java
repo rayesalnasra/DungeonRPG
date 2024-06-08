6a1cf500858744d0a17d936e8fa70521e16325d3
1Billal1 <133625394+1Billal1@users.noreply.github.com>
2024-06-08 19:40:32 +1200

    Setup general settings for the main menu

--- src/DungeonRPG/MainMenu.java
+++ src/DungeonRPG/MainMenu.java
@@ -27,16 +27,19 @@ public class MainMenu extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Dungeon RPG main menu");
+        setPreferredSize(new java.awt.Dimension(600, 500));
+        setResizable(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 587, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 467, Short.MAX_VALUE)
         );
 
         pack();
